ec8094416c9665669aa509603229a6bbdc0cc416
SISU <atgsisu@gmail.com>
2026-04-08 16:15:53 +0900

    fix: 프로그램 종료조건 추가

--- haegeon/main.c
+++ haegeon/main.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void) {
     char sql[1024];
 
     /* stdin에서 한 줄씩 입력받는다. */
     while (fgets(sql, sizeof(sql), stdin) != NULL) {
+        /* 종료 명령이 들어오면 반복을 끝내고 프로그램을 종료한다. */
+        if (strcmp(sql, ".exit\n") == 0 || strcmp(sql, ".quit\n") == 0) {
+            break;
+        }
+
         /* 빈 줄은 무시한다. */
         if (sql[0] == '\n') {
             continue;
